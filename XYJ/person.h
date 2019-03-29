fcd6fef0b618ee781f9e21d27c005823543e6989
user <user@userdeMac-mini.local>
2019-03-29 14:33:30 +0800

    布局适配se修改1.1

---
+++ XYJ/person.h
@@ -0,0 +1,17 @@
+//
+//  person.h
+//  XYJ
+//
+//  Created by user on 2019/3/28.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XYJ/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  XYJ
+//
+//  Created by user on 2019/3/28.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
